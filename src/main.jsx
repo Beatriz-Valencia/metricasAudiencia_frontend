abed5f1b27eb81522fe70efd45b6fc4b0dc969f2
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-09-12 21:16:44 +0200

    creacion presentacion en proceso

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router.dom";
 import "./index.css";
-
-import App from "./App.jsx";
 import Create from "./pages/Create.jsx";
 import Viewer from "./pages/Viewer.jsx";
 import Stats from "/.pages/Stats.jsx";
+import App from "./App.jsx";
+
+//Presentación con título y duración
+function SimpleDeck( durationMs = 20000){
+  return {durationMs: Math.max (durationMs, 3000)} //duración mínima 3 seg
+}
+
+function 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
