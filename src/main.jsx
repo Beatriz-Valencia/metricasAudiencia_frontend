2b01c171b00e9ae598f6154fc232656341502847
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-09-12 20:38:00 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router.dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Create from "./pages/Create.jsx";
+import Viewer from "./pages/Viewer.jsx";
+import Stats from "/.pages/Stats.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Create />} />
+          <Route path="/v/:id" element={<Viewer />} />
+          <Route path="/stats/ïd" element={<Stats />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
